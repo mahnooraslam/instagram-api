12f602881de2d5547ec7e1a21fb362488f50351a
Pravesh <praveshdwivedi999@gmail.com>
2021-06-27 22:26:42 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var express = require("express");
-var ig = require("instagram-node").instagram();
-var app = express();
+const app = require("express")();
+const axios = require("axios");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 3000;
+app.use(cors({ origin: "*" }));
 
 app.get("/", function (req, res) {
-  res.send("Welcome to Instagram api");
+  res.send("Welcome instagram api API");
 });
 
-app.listen(port, function (req, res) {
+app.get("/instagram/:query", function (req, res) {
+  const query = req.params.query;
+
+  axios
+    .get("https://www.instagram.com/explore/tags/" + query + "/?__a=1")
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("An error occurred, please try again later.");
+    });
+});
+
+app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
 });
