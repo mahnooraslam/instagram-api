f4cad61ce55accb5dfa3f04bc29c8e6a61d7f9ac
mahnooraslam <85351008+mahnooraslam@users.noreply.github.com>
2021-06-28 18:55:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,18 @@ const app = require("express")();
 const axios = require("axios");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const url = 'https://instagram85.p.rapidapi.com/account/therealsaifii/info'
+
 app.use(cors({ origin: "*" }));
 
-var options = {
-  method: 'GET',
-  url: 'https://instagram85.p.rapidapi.com/account/" + userName + "/info',
-  headers: {
-    'x-rapidapi-key': 'd54f2722eamshd9a7361b4e9baa2p17c33djsn60caa28f7e25',
-    'x-rapidapi-host': 'instagram85.p.rapidapi.com'
-  }
-};
-
-
-/* axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+app.get("/", function (req, res) {
+  res.send("Welcome instagram api API");
 });
-*/
-axios
-    .request(url,{
-	headers: {
-    'x-rapidapi-key': 'd54f2722eamshd9a7361b4e9baa2p17c33djsn60caa28f7e25',
-    'x-rapidapi-host': 'instagram85.p.rapidapi.com'
-  }
-})
+
+app.get("/instagram/:query", function (req, res) {
+  const query = req.params.query;
+
+  axios
+    .get("https://www.instagram.com/explore/tags/" + query + "/?__a=1")
     .then((response) => {
       res.send(response.data);
     })
@@ -35,15 +21,6 @@ axios
       console.error(err);
       res.status(500).send("An error occurred, please try again later.");
     });
-
-app.get("/", function (req, res) {
-  res.send("Welcome instagram api API");
-});
-
-app.get("/account/:userName", function (req, res) {
-  const query = req.params.query;
-
-  
 });
 
 app.listen(port, function () {
