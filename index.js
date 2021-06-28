0b8d63771f8a101db040a11df400a57298b2ff7b
mahnooraslam <85351008+mahnooraslam@users.noreply.github.com>
2021-06-29 00:15:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var isHTML = RegExp.prototype.test.bind(/(<([^>]+)>)/i);
 
 app.use(cors({ origin: "*" }));
 
-
+/*
 var options = {
   method: 'GET',
   url: 'https://instagram40.p.rapidapi.com/account-info',
   params: {username: 'string(userName)'},
   headers: {'x-rapidapi-key': 'eef6911d3fmshed2a542d137af63p1fd2f2jsn2180f0ddc901','x-rapidapi-host': 'instagram40.p.rapidapi.com'}
   
-};
+}; */
 
 let HeaderOptions = {
   headers: {'x-rapidapi-key': 'd54f2722eamshd9a7361b4e9baa2p17c33djsn60caa28f7e25','x-rapidapi-host': 'instagram-growth.p.rapidapi.com'}
