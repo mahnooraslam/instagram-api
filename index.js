dc28c68ff99edc5589f60a602e50acf0cc08d730
mahnooraslam <85351008+mahnooraslam@users.noreply.github.com>
2021-06-28 22:16:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.use(cors({ origin: "*" }));
 var options = {
   method: 'GET',
   url: 'https://instagram40.p.rapidapi.com/account-info',
-  params: {username: '{userName}',
+  params: {username: '+ userName +',
   headers: {
     'x-rapidapi-key': 'eef6911d3fmshed2a542d137af63p1fd2f2jsn2180f0ddc901',
     'x-rapidapi-host': 'instagram40.p.rapidapi.com'
   }
 };
 
+  
 app.get("/", function (req, res) {
   res.send("Welcome instagram api API");
 });
